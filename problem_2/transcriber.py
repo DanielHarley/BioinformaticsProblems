91ce7258e51891ff0c6aa6220c28c748aec325da
DanielHarley <danielharleygoes@outlook.com>
2023-03-02 20:55:26 -0300

    (code) code refactor

--- problem_2/transcriber.py
+++ problem_2/transcriber.py
@@ -1,25 +1,13 @@
-from typing import List
-
-
 class Transcriber:
 
     def __init__(self, dna_nucleotide_sequence: str):
-        self.dna_nucleotide_list: List[str] = list(dna_nucleotide_sequence)
-        self.rna_nucleotide_list: List[str] = []
+        self.dna_sequence: str = dna_nucleotide_sequence
         self.rna_sequence: str = ''
 
         self.transcribe()
 
     def transcribe(self):
-        for index in range(len(self.dna_nucleotide_list)):
-            dna_nucleotide = self.dna_nucleotide_list[index]
-
-            if dna_nucleotide == "T":
-                self.rna_nucleotide_list.append("U")
-            else:
-                self.rna_nucleotide_list.append(dna_nucleotide)
-
-        self.rna_sequence = ''.join(self.rna_nucleotide_list)
+        self.rna_sequence = self.dna_sequence.replace("T", "U")
 
     def get_rna_sequence(self):
         return self.rna_sequence
